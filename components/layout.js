738d530cda1b1428c22619bd8c64bcc9d6fbc82e
alan <ahw9f@virginia.edu>
2023-09-21 15:59:25 -0400

    fixing basePath ref

--- components/layout.js
+++ components/layout.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
-import { basePath } from '../next.config.js';
+import { basePath } from '../next.config';
 
 const name = 'Your Name';
 export const siteTitle = 'Next.js Sample Website';
